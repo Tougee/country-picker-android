c166dcf5930b08bc8b99647f9fe3c2e0c9e0a9c6
SeniorZhai <developer.zhaitao@gmail.com>
2021-05-13 18:40:33 +0800

    Migrate to AndroidX, update StickyListHeaders

--- countrypicker/src/main/java/com/mukesh/countrypicker/CountryPicker.java
+++ countrypicker/src/main/java/com/mukesh/countrypicker/CountryPicker.java
@@ -7,7 +7,7 @@ import org.json.JSONObject;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.telephony.TelephonyManager;
 import android.text.Editable;
 import android.text.TextUtils;
